43ab408f030a0e39213e9f3c6dcb0ae6e309a8f7
jnieto11 <julionietoduran@gmail.com>
2021-12-19 22:37:23 -0500

    Commit 19/12/2021 noche

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -18,21 +18,173 @@ export class HomeComponent implements OnInit {
     { name: "Cliente" },
     { name: "# pedido" }]
 
-  tempfilters: any = [];
-  items:any;
+  tempfilters: any = [
+    { name: "Marca" },
+    { name: "Articulo" },
+    { name: "Categoria" }
+  ];
+  items: any;
+  tempitems: any = [];
   constructor() { }
 
   ngOnInit(): void {
     console.log(orders);
-    this.items = orders;
-  
+    this.items = [{
+      "DocEntry": 89280,
+      "DocNum": 30042028,
+      "CardCode": "CN-1123088513",
+      "CardName": "BREIDY STIVEN COTRINA RIOS",
+      "GroupCode": 145,
+      "GroupName": "*TIENDAS POS",
+      "SlpCode": 1135,
+      "SlpName": "Servicio Al Cliente",
+      "ItemCode": "SOSWREPSWA2050-001",
+      "ItemName": "REPUES SWA2050 INTERRUPTOR DE POTENCIA",
+      "FirmCode": 117,
+      "FirmName": "SWEISS SOLDADURA",
+      "CategoriaCode": "57",
+      "CategoriaName": "Repuestos",
+      "Subgrupo": "Mma",
+      "UEN": "SOLDADURA",
+      "Quantity": 1.000000,
+      "OpenQty": 1.000000,
+      "LineTotal": 33529.410000,
+      "WhsCode": "32CSCBOG",
+      "PRODUCCION": 0.000000,
+      "TRANSITO": 0.000000,
+      "CANAL": "CENTROS DE SERVICIOS"
+    }];
+
+
   }
 
+  tst = [{
+    desc: 'SlpCode = marca',
+    sub1: [
+      {
+        desc: 'ItemCode =  articulo',
+        sub2: [
+          {
+            desc: ' CategoriaCode = categoria',
+            sub3: [
+              {
+                desc: 'SlpCode = vendedor',
+                sub4: [{
+                  desc: 'CardCode = cliente',
+                  sub5: [
+                    {
+                      desc: 'DocNum = #pedido',
+                    }
+                  ]
+                }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+
+    ]
+  }
+
+    , {
+    desc: 'SlpCode = marca',
+    sub1: [
+      {
+        desc: 'ItemCode =  articulo',
+        sub2: [
+          {
+            desc: ' CategoriaCode = categoria',
+            sub3: [
+              {
+                desc: 'SlpCode = vendedor',
+                sub4: [{
+                  desc: 'CardCode = cliente',
+                  sub5: [
+                    {
+                      desc: 'DocNum = #pedido',
+                    }
+                  ]
+                }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+
+    ]
+  }
+  ]
+
+  // SlpCode = vendedor, 
+  // CardCode = cliente, 
+  // DocNum = nuemero del pedido, 
+  // ItemCode = articulo, 
+  // FirmCode = marca, 
+  // CategoriaCode = categoria
+
+
   selectItem(item: any) {
+
     if (this.tempfilters.indexOf(item) === -1) {
       this.tempfilters.push(item);
     } else {
       this.removeItem(item);
+    }
+
+    // for (let index = 0; index < this.tempfilters.length; index++) {
+    //   const element = this.tempfilters[index];
+    //   console.log(element);
+    //   if (this.tempfilters[0].name === 'Marca') {
+    //   }
+    //   if (this.tempfilters[index].name === 'Articulo') {
+
+    //   }
+    //   if (this.tempfilters[index].name === 'Categoria') {
+
+    //   }
+
+    // }
+
+
+    // for (let index = 0; index < this.items.length; index++) {
+    //   const element = this.items[index];
+    //   console.log(element.SlpCode);
+    //   console.log(element.CardCode);
+    //   console.log(element.DocNum);
+    //   console.log(element.ItemCode);
+    //   console.log(element.FirmCode);
+    //   console.log(element.CategoriaCode);
+    // }
+
+    for (let index = 0; index < this.tst.length; index++) {
+
+
+      console.log(this.tst[index].desc);
+
+
+      for (let index1 = 0; index1 < this.tst[index].sub1.length; index1++) {
+        console.log(this.tst[index].sub1[index1].desc);
+
+        for (let index2 = 0; index2 < this.tst[index].sub1[index1].sub2.length; index2++) {
+          console.log(this.tst[index].sub1[index1].sub2[index2].desc);
+          for (let index3 = 0; index3 < this.tst[index].sub1[index1].sub2[index2].sub3.length; index3++) {
+            console.log(this.tst[index].sub1[index1].sub2[index2].sub3[index3].desc);
+
+            for (let index4 = 0; index4 < this.tst[index].sub1[index1].sub2[index2].sub3[index3].sub4.length; index4++) {
+              console.log(this.tst[index].sub1[index1].sub2[index2].sub3[index3].sub4[index4].desc);
+
+              for (let index5 = 0; index5 < this.tst[index].sub1[index1].sub2[index2].sub3[index3].sub4[index4].sub5.length; index5++) {
+                console.log(this.tst[index].sub1[index1].sub2[index2].sub3[index3].sub4[index4].sub5[index5].desc);
+
+              }
+            }
+          }
+
+        }
+      }
+
     }
   }
 
